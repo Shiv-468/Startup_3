407b3aaed1aa85fcbc22dfa2cd4a271f869473b2
Shiv-468 <shivmuwal.147@gmail.com>
2023-12-27 15:10:39 +0530

    _add font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif'],
+        'sans':['DM Sans','sans-serif']
+      },
+    },
   },
   plugins: [],
 }
